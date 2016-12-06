7dbc256e3e1096b0c2ca549f8f89b7eccb6f5835
Ineke Scheffers <ineke.scheffers@hotmail.com>
2016-12-06 15:00:08 +0100

    implemented spotify authentication with tutorial

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // This section sets up an express server dependent on pug
 const express = require('express')
+var cookieParser = require('cookie-parser')
 const app = express()
 
 // settings for pug
@@ -7,13 +8,16 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'pug')
 
 app.use(express.static(__dirname + '/public'))
+app.use(cookieParser())
 
 console.log('Server is running...')
 
 // require routes
 const indexRouter = require(__dirname + '/routes/index')
+const loginRouter = require(__dirname + '/routes/login')
 
 // use routes
 app.use('/', indexRouter)
+app.use('/', loginRouter)
 
 app.listen(8000)
